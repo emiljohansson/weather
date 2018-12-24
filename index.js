cacdc03640caf9978f5d3d700bab328715e0fd1b
Emil Johansson <emijohansson@deloitte.com>
2018-12-24 11:22:17 +0100

    replace yql with openweathermap

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const YQL = require('yql')
-const unit = 'c'
-const city = 'denver, co'
-const queryString = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '") and u="' + unit + '"'
-const query = new YQL(queryString)
+const request = require('request')
+const unitsCelsius = 'metric'
+const units = unitsCelsius
+const city = 'denver,us'
+const uri = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=8bc5131a596e20bc1f4a0966fda7014d`
 
 const errorMessage = 'Unable to load the weather.'
 
+const getUnit = () => {
+  if (units === unitsCelsius) {
+    return 'C'
+  }
+  return 'F'
+}
+
 module.exports = () => new Promise((resolve, reject) => {
-  query.exec((err, data) => {
-    if (err || !data.query.results) {
+  request(uri, (error, response, body) => {
+    if (error || !body) {
       return reject(errorMessage)
     }
-    const location = data.query.results.channel.location
-    const condition = data.query.results.channel.item.condition
+    const data = JSON.parse(body)
 
     resolve({
-      city: location.city,
-      region: location.region,
-      temp: condition.temp,
-      unit: unit.toUpperCase()
+      city: data.name,
+      country: data.sys.country,
+      temp: data.main.temp,
+      unit: getUnit()
     })
   })
 })
